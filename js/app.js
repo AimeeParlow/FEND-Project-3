b14ef5d54efa5446c4bdbc9ef044214fb716c02e
Aimee-desktop <ai.parlow@gmail.com>
2018-03-27 00:19:52 +0200

    clean files (not yet responsive)

--- js/app.js
+++ js/app.js
@@ -1,39 +1,62 @@
-const myDeck = document.querySelector(".deck");
+/*
+ * variables
+ */
+
+//variables for game base... 
+let myDeck = document.querySelector(".deck");
 let myCards = myDeck.querySelectorAll(".card");
 let cardList = [...myCards];
-let timeArea = document.getElementsByTagName('time')[0];
 let moves = document.getElementById('moves');
+const restartButton = document.getElementById("reset");
 let openedCard = [];
+
+//variables for counting number...
 let matchedCount = 0;
 let clickedCount = 0;
 let failedCount = 0;
-const restartButton = document.getElementById("reset");
 
-
-/*
- * action
- */
-
-$(document).ready(function() {
-	setNewCards();
-});
-
-$(myCards).click(function() {
-	if (firstClick == false){
-	timeCount();
-	firstClick = true;
-	}	
-});
-
-/*
- * timer
- */
-
- //variables for timer...
+//variables for time count...
+let timeArea = document.getElementsByTagName('time')[0];
 let timer;
 let time;
 let count = 0;
 let firstClick = false;
+
+//variables for result area...
+let resultArea = document.getElementById('result-area');
+let resultContent = document.getElementById('result-content');
+let resultCloseButton = document.getElementsByClassName('close')[0];
+let resultText = document.getElementById('result-text');
+let clickedScore = document.getElementById('clicked-score');
+let timeScore = document.getElementById('time-score');
+let starScore = document.getElementById('star-score');//new parent
+let starArea = document.getElementById('star-rank')//old parent
+let gameBoard = document.getElementById('game');
+let playAgainButton = document.getElementById('play-again-button');
+
+/*
+ * functions
+ */
+
+//load the page and start the game...
+$(document).ready(function(){
+	setNewCards();
+});
+
+//reset button to reload the page...
+restartButton.addEventListener("click", reset);
+
+function reset(){
+	location.reload();
+}
+
+//click a card and start counting time but it works only one time...
+$(myCards).click(function(){
+	if (firstClick == false){
+	timeCount();
+	firstClick = true;
+	}
+});
 
 //time count...
 function timeCount(){
@@ -42,15 +65,10 @@ function timeCount(){
 	let min = 0;
 	let hour = 0;
 
-	if(sec > 60){
-		min++;
-	}
-	if(min > 60){
-		hour++;
-	}
-	
+	if(sec > 60){ min++; }
+	if(min > 60){ hour++; }
 	if(sec < 10){ sec = "0" + sec };
-	if(min < 10){ min = "0" + min };	
+	if(min < 10){ min = "0" + min };
 	if(hour < 10){ hour = "0" + hour };
 	
 	time = hour + ":" + min + ":" + sec;
@@ -59,25 +77,12 @@ function timeCount(){
 	timer = setTimeout("timeCount()", 1000);
 }
 
-
 /*
- * Restart button
+ * main actions
  */
 
-restartButton.addEventListener("click", reset);
-
-function reset(){
-	location.reload();
-}
-
-
-
-/*
- * shuffle
- */
- 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
+//card Shuffle (shuffle function from http://stackoverflow.com/a/2450976)
+function shuffle(array){
 	var currentIndex = array.length, temporaryValue, randomIndex;
 
 	while (currentIndex !== 0) {
@@ -91,10 +96,6 @@ function shuffle(array) {
 	return array;
 }
 
-/*
- * to set new cards and to play game...
- */
- 
 //to set new cards as default...
 function setNewCards(){
 	cardList = shuffle(cardList);
@@ -106,22 +107,18 @@ function setNewCards(){
 }
 
 //card actions after clicking it...
-function turnCard() {
-	/*if (this.classList.contains('open')){}
+function turnCard(){
+	if (this.classList.contains('open')){}
 	
 	else {
-		*/
 		this.classList.add('open', 'show');
 		clickedCount ++;
-		console.log("clickedCount " + clickedCount);//to check the count number...will be deleted before submission
 		moves.textContent = clickedCount;
 		openedCard.push(this);
-
-
-		if (openedCard.length === 2) {//once the second card was turned, call matchCheck...
+		if (openedCard.length === 2) {
 			matchCheck();　
-		}
-	//}	
+		}//once the second card was turned, call matchCheck...
+	}
 }
 
 //to check if two opened cards are matched...
@@ -129,94 +126,74 @@ function matchCheck(){
 	if(openedCard[0].lastElementChild.classList.item(1) === openedCard[1].lastElementChild.classList.item(1)){
 		openedCard[0].classList.add('match');
 		openedCard[1].classList.add('match');
-
 		matchedCount++;
-		console.log("yeah!!!!  " + "matchedCount " + matchedCount); //to check the count number...will be deleted before submission
-		openedCard = [];
+		openedCard = [];//to clear the openedCard array...
 		
-		if (matchedCount <8){
-			console.log("matchedCount <8");
-			}
+		if(matchedCount <8){}
 		else{
-			gameClear();
+		setTimeout(function(){gameWon()},450);//you won...
 		}
-		
 	}
 	
 	else {
-		console.log("Not match...");
 		failedCount ++;
-		console.log("failedCount!!!!  " + failedCount); //to check the count number...will be deleted before submission
 		stars();
-		setTimeout(function(){turnBack()},300);
+		setTimeout(function(){turnBack()},300);//to delay time to turn the card...
 	}
 }
 
-//cards not matched and turn back to hide...
-function turnBack(){ 
-		openedCard[0].classList.remove('open', 'show');
-		openedCard[1].classList.remove('open', 'show');
-		openedCard = [];
+//not-matched cards will be turned back to hide...
+function turnBack(){
+	openedCard[0].classList.remove('open', 'show');
+	openedCard[1].classList.remove('open', 'show');
+	openedCard = [];
 }
 
-
-//loosing stars 
+//loosing stars...
 function stars(){
-	if (failedCount == 1){
+	if(failedCount == 4){
 	$('.star:last').remove();
 	}
-	else if  (failedCount == 2){
+	else if(failedCount == 8){
 	$('.star:last').remove();
 	}
-	else if  (failedCount == 3){
+	else if(failedCount == 12){
+	$('.star:last').remove();
 	gameFailed();
-	}	
+	}
 }
 
-// variables for result area...
-let resultArea = document.getElementById('result-area');
-let resultContent = document.getElementById('result-content');
-let resultCloseButton = document.getElementsByClassName('close')[0];
-let resultText = document.getElementById('result-text');
-let clickedScore = document.getElementById('clicked-score');
-let timeScore = document.getElementById('time-score');
-let starScore = document.getElementById('star-score');//new parent
-let starArea = document.getElementById('star-rank')//old parent
-let gameBoard = document.getElementById('game');
-let playAgainButton = document.getElementById('play-again-button');
-
-
-// When the player lost the 3 stars, game is over and call failedResult... 
-function gameFailed() {
+//once the player lost 3 stars, game is over and call failedResult... 
+function gameFailed(){
 	clearTimeout(timer);
 	failedResult();
 	resultArea.style.display = "block";
 }
 
-//to show the failed scoreboard...
+//appear the modal window to show failed result...
 function failedResult(){
 	resultCloseButton.style.display = "block";
-	resultText.innerHTML = "<b>GAME OVER!!!</b>";
+	resultText.innerHTML = "<b>GAME OVER!</b>";
 	clickedScore.textContent = clickedCount + " Moves";
-	timeScore.textContent = "Time " + time;	
+	timeScore.textContent = "Time " + time;
 }
 
-//to close the modal window by clicking x button...
+//to close the modal window by clicking x button and reload...
 resultCloseButton.onclick = function() {
-    resultArea.style.display = "none";
+	resultArea.style.display = "none";
 	reset();
 }
 
-//to close the modal window by clicking anywhere outside of the modal...
-window.onclick = function(event) {
-    if (event.target == resultArea) {
+//to close the modal window by clicking anywhere outside of the modal and reload...
+window.onclick = function(event){
+	if(event.target == resultArea){
 		resultArea.style.display = "none";
 		reset();
-    }
+	}
 }
 
-//to show cleared score...
-function gameClear(){
+//to show winner's result...modal window reforming to as plain page...
+function gameWon(){
 	gameBoard.style.display = "none";
 	resultArea.style.display = "block";
 	playAgainButton.style.display = "block";
@@ -226,7 +203,7 @@ function gameClear(){
 	resultContent.style.padding = "80px 20px 0 20px";
 	resultContent.style.backgroundColor = "#ffe6ff";
 	resultArea.style.backgroundColor = "rgba(0,0,0,0)";
-	resultText.innerHTML = "<b>YOU WON!!!</b>";
+	resultText.innerHTML = "<b>YOU WON!</b>";
 	starScore.style.display = "flex";
 	clickedScore.textContent = clickedCount + " Moves";
 	timeScore.textContent = "Time: " + time;
@@ -234,23 +211,11 @@ function gameClear(){
 	starCount();
 }
 
-
+//to show each rest star...
 function starCount(){
 	while(starArea.childNodes.length > 0){
 		starScore.appendChild(starArea.childNodes[0]);
 	}
 }
 
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+//end of the code...
